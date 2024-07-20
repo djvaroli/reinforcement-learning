9172ba01c940e300c4de4d94cbb13d5adbc5eac7
Daniel Varoli <daniel.varoli@gmail.com>
2024-07-20 17:44:21 -0400

    Added greedy and eps-greedy action selection methods

---
+++ armed_bandit/action/selection/greedy.py
@@ -0,0 +1,27 @@
+from jax import Array
+from jax import numpy as jnp
+from jax import random as jrnd
+
+from .base import ActionSelection
+
+
+class GreeedyActionSelection(ActionSelection):
+    def select(self, random_key: Array, est_action_values: Array) -> Array:
+        val_max = jnp.max(est_action_values)
+        ties = jnp.where(est_action_values == val_max)[0]
+        return jrnd.choice(random_key, ties)
+
+
+class EpsilonGreedyActionSelection(ActionSelection):
+    def __init__(self, epsilon: float):
+        if not 0 <= epsilon <= 1:
+            raise ValueError("Epsilon must be between 0 and 1.")
+
+        self.epsilon = epsilon
+
+    def select(self, random_key: Array, est_action_values: Array) -> Array:
+        # sample from a uniform distribution
+        if jrnd.uniform(random_key, (1,)) < self.epsilon:
+            return jrnd.choice(random_key, est_action_values)
+
+        return super().select(random_key, est_action_values)
