2097e4ed2e450c7bb257f5d026fa977291d32015
Daniel Varoli <daniel.varoli@gmail.com>
2024-07-20 20:15:42 -0400

    refactor bandit script to allow specifying action selection method

--- armed_bandit/run_k_armed_bandit.py
+++ armed_bandit/run_k_armed_bandit.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 import seaborn as sns
+from action.selection import EpsilonGreedyActionSelection, GreeedyActionSelection
 from bandit import KArmedBandit
 from jax import Array
 from jax import numpy as jnp
@@ -13,6 +14,46 @@ from matplotlib import pyplot as plt
 from tqdm.auto import tqdm
 
 sns.set_theme()
+
+
+class ExperimentDataContainer:
+    def __init__(
+        self,
+        n_experiments: int,
+        n_steps: int,
+    ):
+        self.reward_history = jnp.zeros((n_experiments, n_steps))
+        self.selected_action_history = jnp.zeros((n_experiments, n_steps), dtype=int)
+        self.optimal_action_history = jnp.zeros((n_experiments,), dtype=int)
+
+    @property
+    def n_experiments(self) -> int:
+        return self.reward_history.shape[0]
+
+    @property
+    def n_steps(self) -> int:
+        return self.reward_history.shape[1]
+
+    def update_rewards(self, experiment: int, step: int, reward: Array) -> None:
+        self.reward_history = self.reward_history.at[experiment, step].set(reward)
+
+    def update_selected_actions(self, experiment: int, step: int, action: int) -> None:
+        self.selected_action_history = self.selected_action_history.at[
+            experiment, step
+        ].set(action)
+
+    def update_optimal_action(self, experiment: int, optimal_action: int) -> None:
+        self.optimal_action_history = self.optimal_action_history.at[experiment].set(
+            optimal_action
+        )
+
+    def save(self, path: str) -> None:
+        jnp.savez(
+            path,
+            reward_history=self.reward_history,
+            selected_action_history=self.selected_action_history,
+            optimal_action_history=self.optimal_action_history,
+        )
 
 
 def generate_experiment_id(length: int = 8) -> str:
@@ -53,68 +94,28 @@ def average_and_plot(
 
 
 def run_experiment(
-    n_arms: int,
-    n_steps: int,
+    bandit: KArmedBandit,
     n_experiments: int,
-    init_Q_est: float,
-    experiment_dir: str,
-    random_seed: int | None = None,
-):
-    bandit = KArmedBandit(n_arms, init_Q_est, random_seed)
-
-    reward_history = jnp.zeros((n_experiments, n_steps))
-    selected_action_history = jnp.zeros((n_experiments, n_steps), dtype=int)
-    optimal_action_history = jnp.zeros((n_experiments,), dtype=int)
+    n_steps: int,
+) -> ExperimentDataContainer:
+    experiment_data = ExperimentDataContainer(n_experiments, n_steps)
 
     with tqdm(range(n_experiments), total=n_experiments) as pbar:
         for experiment in range(n_experiments):
             pbar.set_description(f"Run {experiment + 1}/{n_experiments}")
 
-            # initialize the bandit. Automatically updates the random key such that
-            # the next run will have a different starting random key
+            # re-initialize bandit for next experiment run. Automatically updates random state
             bandit.reinit()
-            optimal_action_history = optimal_action_history.at[experiment].set(
-                bandit.optimal_action
-            )
+            experiment_data.update_optimal_action(experiment, bandit.optimal_action)
 
             for t in range(n_steps):
                 reward, action = bandit.pull()
-
-                reward_history = reward_history.at[experiment, t].set(reward)
-                selected_action_history = selected_action_history.at[experiment, t].set(
-                    action
-                )
+                experiment_data.update_rewards(experiment, t, reward)
+                experiment_data.update_selected_actions(experiment, t, action)
 
             pbar.update(1)
 
-    experiment_dir = Path(experiment_dir).resolve()
-    average_and_plot(
-        reward_history,
-        "Steps",
-        "Average Reward",
-        label=f"Runs={n_experiments}, K={n_arms}, Q0={init_Q_est}",
-        save_as=experiment_dir / "average_reward.png",
-    )
-
-    was_optimal_policy_selected = (
-        selected_action_history == optimal_action_history.reshape(-1, 1)
-    )
-    average_and_plot(
-        was_optimal_policy_selected,
-        "Steps",
-        "% Optimal Action",
-        label=f"Runs={n_experiments}, K={n_arms}, Q0={init_Q_est}",
-        save_as=experiment_dir / "optimal_action.png",
-    )
-
-    bandit.save(experiment_dir / "bandit_state.npz")
-
-    jnp.savez(
-        experiment_dir / "history_arrays.npz",
-        reward_history=reward_history,
-        action_history=selected_action_history,
-        optimal_action_history=optimal_action_history,
-    )
+    return experiment_data
 
 
 if __name__ == "__main__":
@@ -122,6 +123,13 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--n_arms", "-k", type=int, help="Number of arms in the bandit.", required=True
+    )
+    parser.add_argument(
+        "--action_selector",
+        "-a",
+        type=str,
+        help="Action selection method to use.",
+        required=True,
     )
     parser.add_argument(
         "--n_steps",
@@ -134,7 +142,7 @@ if __name__ == "__main__":
         "--n_runs", "-r", type=int, default=1, help="Number of runs to average over."
     )
     parser.add_argument(
-        "--av_bias",
+        "--action_value_bias",
         "-b",
         type=float,
         default=0.0,
@@ -150,11 +158,49 @@ if __name__ == "__main__":
     experiment_dir = Path(experiment_id).resolve()
     experiment_dir.mkdir(exist_ok=True)
 
-    start_time = time.perf_counter()
-    run_experiment(
-        args.n_arms, args.n_steps, args.n_runs, args.av_bias, experiment_dir, args.seed
+    action_selection_method = eval(args.action_selector)
+    print(action_selection_method)
+
+    bandit = KArmedBandit(
+        args.n_arms, action_selection_method, args.action_value_bias, args.seed
     )
+
+    start_time = time.perf_counter()
+    experiment_data = run_experiment(
+        bandit,
+        args.n_runs,
+        args.n_steps,
+    )
+
     end_time = time.perf_counter()
+
+    label = ""
+    for key, value in vars(args).items():
+        label += f"{key}={value}, "
+
+    average_and_plot(
+        experiment_data.reward_history,
+        "Steps",
+        "Average Reward",
+        label=label,
+        save_as=experiment_dir / "average_reward.png",
+    )
+
+    was_optimal_policy_selected = (
+        experiment_data.selected_action_history
+        == experiment_data.optimal_action_history.reshape(-1, 1)
+    )
+
+    average_and_plot(
+        was_optimal_policy_selected,
+        "Steps",
+        "% Optimal Action",
+        label=label,
+        save_as=experiment_dir / "optimal_action.png",
+    )
+
+    bandit.save(experiment_dir / "bandit_state.npz")
+    experiment_data.save(experiment_dir / "experiment_data.npz")
 
     config = vars(args)
     config["experiment_id"] = experiment_id
